71507055f415524eb6d63706a61fc10f970bbc36
DavidGarciaGiron <david.garcia@tecsup.edu.pe>
2019-03-04 22:46:40 -0500

    first commit

---
+++ Tarea2-Laboratorio2/Tarea2-Laboratorio2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2_Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido Usuario");
+            Console.WriteLine("==================");
+            Console.WriteLine("");
+            Console.WriteLine("Programa para hayar el Perimetro y Area de un rectangulo en un Plano Cartesiano.");
+            // Crear Rectangulo
+            Rectangulo rectangulo = new Rectangulo();
+
+            Console.WriteLine("Ingrese Punto 1 en el Eje X:");
+            double p1X = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P1EjeX = p1X;
+            Console.WriteLine("Ingrese Punto 1 en el Eje Y:");
+            double p1Y = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P1EjeY = p1Y;
+            Console.WriteLine("Ingrese Punto 2 en el Eje X:");
+            double p2X = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P2EjeX = p2X;
+            Console.WriteLine("Ingrese Punto 2 en el Eje Y:");
+            double p2Y = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P2EjeY = p1Y;
+            Console.WriteLine("Ingrese Punto 3 en el Eje X:");
+            double p3X = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P3EjeX = p3X;
+            Console.WriteLine("Ingrese Punto 3 en el Eje Y:");
+            double p3Y = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P3EjeY = p3Y;
+            Console.WriteLine("Ingrese Punto 4 en el Eje X:");
+            double p4X = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P4EjeX = p4X;
+            Console.WriteLine("Ingrese Punto 4 en el Eje Y:");
+            double p4Y = Convert.ToDouble(Console.ReadLine());
+            rectangulo.P4EjeY = p4Y;
+
+            double Perimetro;
+            double Area;
+
+            Perimetro = rectangulo.CalcularPerimetro();
+            Area = rectangulo.CalcularArea();
+
+            Console.WriteLine(string.Concat("El Perimetro de el Rectangulo es: [", Perimetro.ToString(), "]"));
+            Console.WriteLine("===");
+            Console.WriteLine(string.Concat("El Area de el Rectangulo es: [", Area.ToString(), "]"));
+            Console.WriteLine("===");
+            Console.WriteLine("Gracias por Usar el Programa");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Tarea2-Laboratorio2/Tarea2-Laboratorio2/Rectangulo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2_Laboratorio2
+{
+    class Rectangulo
+    {
+        public int Id { get; set; }
+        public double P1EjeX { get; set; }
+        public double P1EjeY { get; set; }
+        public double P2EjeX { get; set; }
+        public double P2EjeY { get; set; }
+        public double P3EjeX { get; set; }
+        public double P3EjeY { get; set; }
+        public double P4EjeX { get; set; }
+        public double P4EjeY { get; set; }
+
+        public double CalcularPerimetro()
+        {
+            double d1 = (double)(((P2EjeX-P1EjeX)*(P2EjeX - P1EjeX))+((P2EjeY-P1EjeY)*(P2EjeY - P1EjeY)));
+            double Rd1 = Math.Sqrt(d1);
+            double d2 = (double)(((P3EjeX-P2EjeX)*(P3EjeX - P2EjeX))+((P3EjeY-P2EjeY)*(P3EjeY - P2EjeY)));
+            double Rd2 = Math.Sqrt(d2);
+            double d3 = (double)(((P4EjeX-P3EjeX)*(P4EjeX - P3EjeX))+((P4EjeY-P3EjeY)*(P4EjeY - P3EjeY)));
+            double Rd3 = Math.Sqrt(d3);
+            double d4 = (double)(((P1EjeX-P4EjeX)*(P1EjeX - P4EjeX))+((P1EjeY-P4EjeY)*(P1EjeY - P4EjeY)));
+            double Rd4 = Math.Sqrt(d4);
+            double perimetro = 2*(Rd1+Rd2);
+            return perimetro;
+        }
+
+        public double CalcularArea()
+        {
+            double d1 = (double)(((P2EjeX - P1EjeX) * (P2EjeX - P1EjeX)) + ((P2EjeY - P1EjeY) * (P2EjeY - P1EjeY)));
+            double Rd1 = Math.Sqrt(d1);
+            double d2 = (double)(((P3EjeX - P2EjeX) * (P3EjeX - P2EjeX)) + ((P3EjeY - P2EjeY) * (P3EjeY - P2EjeY)));
+            double Rd2 = Math.Sqrt(d2);
+            double d3 = (double)(((P4EjeX - P3EjeX) * (P4EjeX - P3EjeX)) + ((P4EjeY - P3EjeY) * (P4EjeY - P3EjeY)));
+            double Rd3 = Math.Sqrt(d3);
+            double d4 = (double)(((P1EjeX - P4EjeX) * (P1EjeX - P4EjeX)) + ((P1EjeY - P4EjeY) * (P1EjeY - P4EjeY)));
+            double Rd4 = Math.Sqrt(d4);
+            double area = (Rd1) * (Rd2);
+            return area;
+        }
+    }
+}
